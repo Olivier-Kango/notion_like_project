dd37c2a0a095221ab07bfaa69423c6b07fad042b
Olivier-Kango <olivierkango@gmail.com>
2023-01-20 20:30:13 -0800

    Create index.jsx file

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import '../src/styling/index.scss';
+import './styling/index.scss';
 import App from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
