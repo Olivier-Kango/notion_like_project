e4fdb07bc8d49e0fb3fdc2fd3185272f5bb60300
Olivier-Kango <olivierkango@gmail.com>
2024-01-04 09:05:17 +0200

    update input

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,20 +3,6 @@ import EditablePage from './Page';
 
 const App = () => (
   <div className="App">
-    <h1>Front-end developer test project</h1>
-    <hr />
-    <p className="welcome">
-      <span role="img" aria-label="greetings" className="Emoji">
-        👋
-      </span>
-      {' '}
-      Welcome to this App !
-      You can add content below. Type
-      {' '}
-      <span className="Code">/</span>
-      {' '}
-      to see available elements.
-    </p>
     <EditablePage />
   </div>
 );
